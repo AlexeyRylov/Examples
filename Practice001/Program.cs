fcc92e529970ff9c2a9a925d1609bde9923278d8
alexey <aarylov@gmail.com>
2022-07-09 18:30:46 +0300

    Квадрат числа

---
+++ Practice001/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите целое число: ");
+int num;
+String s = Console.ReadLine();
+num = int.Parse(s);
+int result = num * num;
+Console.WriteLine("Квадрат вашего числа равен: " + result);
