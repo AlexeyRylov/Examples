3df83c4f48d3fd38b1d4dd9d8f49d2ea5fb95a5e
alexey <aarylov@gmail.com>
2022-07-12 22:46:36 +0500

    Задача 1 семинара 2

---
+++ Practice005/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Напишите программу, которая выводит слуайное число из отрезка [10, 100] и показывает наибольшую цифру числа
+*/
+
+int num = new Random().Next(10, 100);
+Console.WriteLine("Random number: " + num);
+
+int firstDigit = num / 10;
+int secondDigit = num % 10;
+
+Console.WriteLine("Result " + getMax(firstDigit, secondDigit));
+
+int getMax(int a, int b)
+{
+    if (a > b)
+    {
+        return a;
+    }
+    else
+    {
+        return b;
+    }
+}
