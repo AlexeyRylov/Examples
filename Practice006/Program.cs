5839cc30afdd8bfcda898135fd1004957733961f
alexey <aarylov@gmail.com>
2022-07-12 22:55:24 +0500

    Задача 2 семинара 2

---
+++ Practice006/Program.cs
@@ -0,0 +1,11 @@
+﻿/*
+Напишите программу, которая выводит трехзначное целое число и удаляет вторую цифру этого числа
+*/
+
+int a = new Random().Next(100, 1000);
+
+int digitFirst = a / 100;
+int digitThird = a % 10;
+
+Console.WriteLine("случайное число: " + a);
+Console.WriteLine(digitFirst*10 + digitThird);
