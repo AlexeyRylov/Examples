048e2526d31b09b201372fbf63ee88d54985b514
alexey <aarylov@gmail.com>
2022-07-12 23:38:02 +0500

    Задача 4 семинара 2 с методом

--- Practice008/Program.cs
+++ Practice008/Program.cs
@@ -4,15 +4,30 @@
 46 -> нет 
 161 -> да
 */
-
 Console.Write("Введите число : ");
 int a = int.Parse(Console.ReadLine());
+Func(a);
 
+void Func(int number)
+{
+    if ((number % 7 == 0) && (number % 23 == 0))
+    {
+        Console.WriteLine("да");
+    }
+    else
+    {
+        Console.WriteLine("нет");
+    }
+}
+
+
+/*
 if ((a % 7 == 0) && (a % 23 == 0)) //&& - если первое условие не выполнено, то второе и не проводится
 {
     Console.WriteLine("да");
-} else
+}
+else
 {
     Console.WriteLine("нет");
 }
-
+*/
